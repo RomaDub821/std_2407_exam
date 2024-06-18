e4f1984108ce9ecec183fddf2e7eaafa819e026c
RomaDub281 <roma1105040@mail.ru>
2024-06-18 05:48:58 +0300

    AAAA

--- RDExam/config.py
+++ RDExam/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'you_will_never_guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2407_exam:Duba13579@std-mysql.ist.mospolytech.ru/std_2407_exam'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///library.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/images')
